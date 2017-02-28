355c044e4b771436fd3423bd61d27fad1c56bc14
David D'Haiti <sddhaiti@gmail.com>
2017-02-27 20:20:10 -0500

    update: Proccess and puropse

--- Main.py
+++ Main.py
@@ -5,21 +5,23 @@ from compute import compute
 app = Flask(__name__)
 
 # Model
+    # Here Q and R will be the two numbers coming in from the user
 class InputForm(Form):
     q = FloatField(validators=[validators.InputRequired()])
     r = FloatField(validators=[validators.InputRequired()])
 
 # View
-@app.route('/hw1', methods=['GET', 'POST'])
+@app.route('/hw1', methods=['GET', 'POST']) #app.route displays the path that is used to get to the file and also it contains the methods of how the file will be proccessed 
 def index():
-    form = InputForm(request.form)
-    if request.method == 'POST' and form.validate():
-        q = form.q.data
-        r = form.r.data
-        s = compute(q,r)
-        return render_template("view_output.html", form=form, s=s)
+    form = InputForm(request.form) 
+    if request.method == 'POST' and form.validate(): 
+        q = form.q.data #The number inputed from the user is saved here 
+        r = form.r.data #The number inputed from the user is saved here 
+        
+        s = compute(q,r) #compute is a funciton from a seperate file 
+        return render_template("view_output.html", form=form, s=s) #Outputs the the html file with the inputed and computed information  
     else:
-        return render_template("view_input.html", form=form)
+        return render_template("view_input.html", form=form) #Outputs to the screen the initial html file requesting the input values fromt the user
 
-if __name__ == '__main__':
+if __name__ == '__main__': #This is the entry point 
     app.run(debug=True)
